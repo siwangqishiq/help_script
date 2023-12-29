943b4958a45fe317df7bde482aebb01494c96368
siwangqishiq <525647740@qq.com>
2023-12-29 22:25:28 +0800

    update fetch script

--- catch_beauty.py
+++ catch_beauty.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import os;
 
-FETCH_URL = "https://www.tuiimg.com/meinv/3085/"
-DIR = "IMG2"
+FETCH_URL = "https://www.tuiimg.com/meinv/3069/"
+# DIR = "3057"
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+DIR_NAME = FETCH_URL.split("/")[-2]
 
 def fetch_pics(url):
     print(f"抓取 {url}")
@@ -16,6 +18,9 @@ def fetch_pics(url):
     if resp.status_code != 200:
         print(f"catch error {url}")
         return
+    
+    # DIR_NAME = url.split("/")[-2]
+    print(f"DIR_NAME: {DIR_NAME}")
 
     soup = BeautifulSoup(resp.text , "html.parser")
 
@@ -45,17 +50,17 @@ def fetch_pics(url):
 
     for index in range(total_image_count) :
         img_url = fileurl + str(index + 1) + ".jpg" 
-        print(img_url)
+        print(img_url + " progress(%d/%d)"%(index,total_image_count))
         download_pic(img_url , "img_" + str(index + 1) + ".jpg" )
 
     
 def download_pic(resUrl , filename):
     print(f"download {resUrl}")
-    if not os.path.exists(DIR):
-        os.makedirs(DIR)
+    if not os.path.exists(DIR_NAME):
+        os.makedirs(DIR_NAME)
     r = requests.get(resUrl, stream=True)
     if r.status_code == 200:
-        with open(f"{DIR}/{filename}" , "wb") as f:
+        with open(f"{DIR_NAME}/{filename}" , "wb") as f:
            f.write(r.content)
         print("dowload " + resUrl +" success! to " + f.name) 
     else:
