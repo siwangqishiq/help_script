c7d494c4ba04042edb1d61362345bb4f1a74afe5
siwangqishiq <525647740@qq.com>
2024-01-01 21:36:24 +0800

    global vars

--- catch_beauty.py
+++ catch_beauty.py
@@ -4,26 +4,27 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import os;
 
-FETCH_URL = "https://www.tuiimg.com/meinv/3097/"
+FETCH_URL = "https://www.tuiimg.com/meinv/3099/"
 # DIR = "3057"
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 DIR_NAME = ""
 
+
 def fetch_pics(url):
     print(f"抓取 {url}")
-    resp = requests.get(url , headers={
+    resp = requests.get(url, headers={
         'User-Agent': UA
     })
     if resp.status_code != 200:
         print(f"catch error {url}")
         return
-    
+
     global DIR_NAME
     DIR_NAME = url.split("/")[-2]
     print(f"DIR_NAME: {DIR_NAME}")
 
-    soup = BeautifulSoup(resp.text , "html.parser")
+    soup = BeautifulSoup(resp.text, "html.parser")
 
     content = soup.find(id="content")
     first_img = content.find("img")
@@ -34,7 +35,7 @@ def fetch_pics(url):
 
     for sc in all_scripts:
         script_content = sc.text
-        if "_pd =" in  script_content:
+        if "_pd =" in script_content:
             # print(script_content)
             left = script_content.find("[") + 1
             right = script_content.find("]")
@@ -43,30 +44,29 @@ def fetch_pics(url):
             for info in page_info.split(","):
                 pd.append(info.strip(" \""))
             print(pd)
-        
+
     fileurl = 'https://i.tuiimg.net/' + pd[1];
-    
+
     total_image_count = int(pd[3])
     print(f"total image count : {total_image_count}")
 
-    for index in range(total_image_count) :
-        img_url = fileurl + str(index + 1) + ".jpg" 
-        print(img_url + " progress(%d/%d)"%(index,total_image_count))
-        download_pic(img_url , "img_" + str(index + 1) + ".jpg" )
+    for index in range(total_image_count):
+        img_url = fileurl + str(index + 1) + ".jpg"
+        print(img_url + " progress(%d/%d)" % (index, total_image_count))
+        download_pic(img_url, "img_" + str(index + 1) + ".jpg")
 
-    
-def download_pic(resUrl , filename):
+
+def download_pic(resUrl, filename):
     print(f"download {resUrl}")
     if not os.path.exists(DIR_NAME):
         os.makedirs(DIR_NAME)
     r = requests.get(resUrl, stream=True)
     if r.status_code == 200:
-        with open(f"{DIR_NAME}/{filename}" , "wb") as f:
-           f.write(r.content)
-        print("dowload " + resUrl +" success! to " + f.name) 
+        with open(f"{DIR_NAME}/{filename}", "wb") as f:
+            f.write(r.content)
+        print("dowload " + resUrl + " success! to " + f.name)
     else:
-        print("dowload " + resUrl +" failed!") 
-    
+        print("dowload " + resUrl + " failed!")
 
 
 if __name__ == "__main__":
