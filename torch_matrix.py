541723ed60649f7a40aee0560e3f0be0cd01ee7b
panyi <panyi@yixin.im>
2025-02-27 19:02:14 +0800

    update time

--- torch_matrix.py
+++ torch_matrix.py
@@ -1,4 +1,6 @@
 import torch
+import time
+
 
 if(torch.cuda.is_available()):
     print("CUDA can available")
@@ -7,7 +9,7 @@ else:
 
 deviceCpu = torch.device("cpu");
 
-dimSize = 100
+dimSize = 10000
 a = torch.rand(dimSize,dimSize , device=deviceCpu)
 print("a: ", a)
 print("device: ", a.device)
@@ -15,8 +17,12 @@ print("device: ", a.device)
 b = torch.eye(dimSize,dimSize , device=deviceCpu)
 print("b: " , b)
 
+start_time = time.time_ns()
 c = torch.matmul(a , b)
+end_time = time.time_ns()
 print(c)
+
+print(f"deltaTime : {(end_time - start_time)/1000000} ms")
 
 
 
